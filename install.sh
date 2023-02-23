33fc0164f39e9e8144774381afaec9aa3ebfd23d
hofzzy <hofzzy@gmail.com>
2023-02-23 18:16:58 +0900

    Add VSCode settings

--- install.sh
+++ install.sh
@@ -2,16 +2,26 @@
 
 DOTFILES_HOME=$(pwd)
 CONFIG_HOME="config"
+VS_CODE="vscode"
 
 configs=(
-    zsh
     git
+    $VS_CODE
+    zsh
 )
+
+create_symbolic_links () {
+    for file in `ls -A | xargs readlink -f`; do
+        echo "Create symbolic link..."
+        ln -svf $file "$1"
+    done
+}
 
 for config in "${configs[@]}"; do
     cd $DOTFILES_HOME/$CONFIG_HOME/$config
-    for file in `ls -A | xargs readlink -f`; do
-        echo "Create symbolic link..."
-        ln -svf $file $HOME
-    done
+    if [ $config = $VS_CODE ]; then
+        create_symbolic_links $HOME/Library/Application\ Support/Code/User
+        continue
+    fi
+    create_symbolic_links $HOME
 done
