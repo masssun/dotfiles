4a1e16fa6c9024346c3ea6b9c4fc7e3a656d67a9
hofzzy <hofzzy@gmail.com>
2023-02-23 19:16:19 +0900

    Add alias

--- config/zsh/rc/alias.rc.zsh
+++ config/zsh/rc/alias.rc.zsh
@@ -1,8 +1,12 @@
 # Git
 alias g="git"
-alias gcd='cd $(ghq root)/$(ghq list | peco)'
 alias gbda="git branch | xargs git branch -D"
 alias -g B='`git branch -a | peco --prompt "GIT BRANCH>" | head -n 1 | sed -e "s/^\*\s*//g"`'
+
+# ghq
+alias ghg='ghq get'
+alias ghgp='ghq get -p'
+alias gcd='cd $(ghq root)/$(ghq list | peco)'
 
 # Flutter
 alias flutter="fvm flutter"
@@ -11,3 +15,6 @@ alias fbuild="flutter pub run build_runner build --delete-conflicting-outputs"
 
 # Shell
 alias relogin="exec $SHELL -l"
+
+alias rr="rm -r"
+alias rrf="rm -rf"
