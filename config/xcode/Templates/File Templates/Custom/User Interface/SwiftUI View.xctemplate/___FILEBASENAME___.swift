2200e5166e86d0c15ca6d7f9139ed46ce4153ce3
masssun <hofzzy@gmail.com>
2025-10-09 22:56:13 +0900

    feat: Xcode カスタムファイルテンプレートを追加
    - ヘッダーコメントなしの Swift File テンプレートを追加
    - ヘッダーコメントなしの SwiftUI View テンプレートを追加
    - install スクリプトに Xcode テンプレートのシンボリックリンク作成処理を追加
    - テンプレートは ~/Library/Developer/Xcode/Templates/File Templates/Custom にリンクされる

---
+++ config/xcode/Templates/File Templates/Custom/User Interface/SwiftUI View.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct ___FILEBASENAMEASIDENTIFIER___: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ___FILEBASENAMEASIDENTIFIER___()
+}
